d574127d1ace8283832d347fe3701c03e6c3e32c
DaveO <dufferdo2@gmail.com>
2020-01-14 12:58:50 -0800

    Added jsoneditor to koa demo server

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,11 +6,13 @@ const combineRouters = require("koa-combine-routers");
 const dodexRouter = require("./dodex");
 const testRouter = require("./test");
 const rootRouter = require("./root");
+const bootRouter = require("./boot");
 
 const routers = combineRouters(
   dodexRouter.router,
   testRouter,
-  rootRouter
+  rootRouter,
+  bootRouter
 );
 
 exports.dodexRouter = dodexRouter;
